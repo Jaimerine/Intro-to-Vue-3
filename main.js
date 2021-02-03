45b1a0775242468e55486874256ed5fe36813b69
jaimerobbins <robbins_jaime@hotmail.com>
2021-02-03 09:52:34 -0500

    lesson 2 updates

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    //options object - mandatory, at least empty object
+    //data: function () { <--longhand version, ES6 shorthand below
+    data() {
+        return {
+            product: 'Socks',
+            description: 'They are socks!'
+        }
+    }
+})
+
+
